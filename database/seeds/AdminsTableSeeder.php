0f78eef5d746bfcec28aaa6f56bba02ef707722b
maohhgg <maohhgg@gmail.com>
2019-06-01 13:10:34 +0800

    bug

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -14,6 +14,8 @@ class AdminsTableSeeder extends Seeder
         DB::table('admins')->insert([
             'name' => 'admin',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
     }
 }
